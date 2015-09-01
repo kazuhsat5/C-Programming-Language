d176fddc2c8abf7d21a1bc6ccbd9229f39a927cb
佐藤　和大 <kazuhsat@MBP-15JAC-671.local>
2015-09-01 23:34:08 +0900

    1-13.2

---
+++ Chapter_1/1-13.2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int dir, c, cnt, i;
+
+    printf("direction[v/h]\n");
+
+    dir = getchar();
+
+    if (dir != 'v' && dir != 'h') {
+        printf("Error: invalid\n");
+        return 0;
+    }
+
+    printf("please, input word.\n");
+
+    cnt = 0;
+    while ((c = getchar()) != EOF) {
+        if (c != ' ' && c != '\t' && c != '\n') {
+            ++cnt;
+        }
+    }
+
+    if (dir == 'v') {
+         for (i=0; i<cnt; i++) {
+            printf("*\n");
+        }
+    } else {
+        for (i=0; i<cnt; i++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
